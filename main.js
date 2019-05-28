0cf41804a1e60ea9e21741cbdb26c6bde5d3f611
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-05-28 14:27:42 -0400

    Object Class Projec

---
+++ main.js
@@ -0,0 +1,30 @@
+function Student(firstName, lastName, term){
+    this.firstName  = firstName;
+    this.LastName   = lastName;
+    this.term       = term;
+}
+
+function Instructor(firstName, lastName, term){
+    this.firstName  = firstName;
+    this.LastName   = lastName;
+    this.term       = term;
+}
+
+let school = new Object();
+let student = new Object();
+let instructor = new Object();
+
+
+school.student1 = new Student("Miguel", "Fernandez", "Term2");
+school.student2 = new Student("Keevi", "Hicks", "Term2");
+school.student3 = new Student("David", "Mora", "Term2");
+school.student4 = new Student("Manish", "Singh", "Term2");
+school.student5 = new Student("Paul", "", "Term 2");
+
+school.instructor1 = new Instructor("Yury", "Shkoda", "Term2");
+school.instructor2 = new Instructor("Ginny", "Gu Tang", "Term2");
+school.instructor3 = new Instructor("Colin", "Jaffe", "Term1");
+
+
+console.log(school);
+
